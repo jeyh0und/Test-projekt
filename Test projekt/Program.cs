40aa7fd512fc94211f007f0dbb37daf546658b45
jeyh0und <jeyhun.mansurov@outlook.com>
2022-09-05 16:17:44 +0200

    Added Console.Clear code to the project

--- Test projekt/Program.cs
+++ Test projekt/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Vad heter du?");
 string name = Console.ReadLine();
+Console.Clear();    
 Console.WriteLine("Hej, " + name);
